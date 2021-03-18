de645b449b63b50146817fbb5b288e4f539890dc
HanaLevak <hanna.levak@gmail.com>
2021-03-18 18:02:04 +0000

    gh_pages_repo
    Signed-off-by: HanaLevak <hanna.levak@gmail.com>

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,41 @@
+function userInformationHTML(user){
+    return `
+    <h2>${user.name}
+    <span class="small-name">
+    (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+    </span>
+    </h2>
+    <div class="gh-content">
+    <div class="gh-avatar">
+    <a href="${user.html_url} target="_blank">
+<img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+</a>
+</div>
+    <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+    </div>`;
+}
+
+function repoInformationHTML(repos){
+    if(repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+}
+
+var listItemHTML = repos.map(function(repo) {
+    return `<li>
+                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+});
+
+return `<div class="clearfix repo-list">
+<p>
+<strong>Repo List:</strong>
+</p>
+<ul>
+${listItemHTML.join("\n")}
+</ul>
+</div>`;
+}
+
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
@@ -10,4 +48,27 @@ function fetchGitHubInformation(event) {
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
+    
+    
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+
+    ).then( 
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        },
+        
+        function(errorResponse) {
+            if (errorResponse.status === 404 ){
+                $("#gh-user-data").html(`<h2>No info for user ${username}</h2>`);
+            } else{
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
     }
